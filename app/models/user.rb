3df081f5b8e5da851cd27548febdb1e060794e0c
Alan Fidelino <afidedev26@gmail.com>
2016-03-30 21:01:43 +1100

    Update header

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :time_zone
+  # validates_presence_of :time_zone
 end
