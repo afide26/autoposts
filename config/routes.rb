de15f8f49d09f81cc32e1336d6c9d71664d70391
Alan Fidelino <afidedev26@gmail.com>
2016-03-31 18:51:30 +1100

    Modified extended routes for registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controller: {registrations: 'registrations'}
+  devise_for :users, controllers: {registrations: 'registrations'}
   get 'pages/home'
   root 'pages#home'
   get 'dashboard' => 'pages#dashboard'
